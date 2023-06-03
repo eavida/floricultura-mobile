f6ea0df3764fe68f17021b6bd77ad51fdcb38e8b
eavida <marcosjr268@gmail.com>
2023-06-02 22:50:46 -0300

    Retirado voltar para tela de login

--- floricultura/lib/screens/home.dart
+++ floricultura/lib/screens/home.dart
@@ -14,35 +14,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/background.jpg"),
-              fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/background.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icone(iconData: Icons.home),
-                WidgetTexto(
-                  text: 'Floricultura',
-                  tamanho: 30,
-                  alignment: Alignment.center,
-                ),
-                PreviewItens(text: 'Orquídeas'),
-                PreviewItens(text: 'Rosas'),
-                PreviewItens(text: 'Margaridas'),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icone(iconData: Icons.home),
+                  WidgetTexto(
+                    text: 'Floricultura',
+                    tamanho: 30,
+                    alignment: Alignment.center,
+                  ),
+                  PreviewItens(text: 'Orquídeas'),
+                  PreviewItens(text: 'Rosas'),
+                  PreviewItens(text: 'Margaridas'),
+                ],
+              ),
             ),
           ),
         ),
+        bottomNavigationBar: const NavBar(),
       ),
-      bottomNavigationBar: const NavBar(),
     );
   }
 }
